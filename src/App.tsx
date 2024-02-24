b9ffa03a199913369a0686fa3adaae313a6631f9
Olabisi Olaoye <olabisi.olaoye@cyber.cyberspace.net.ng>
2024-02-24 19:25:16 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <h1>DevRepo - The ultimate Dev Store</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <h1>DevRepo - The ultimate Dev Store</h1>
+    </div>
+  );
+}
